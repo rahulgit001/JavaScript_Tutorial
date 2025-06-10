6a1b75d53eb00987ac0b6e26d4c8c89eda73760b
Rahul kumar <131306879+rahulgit001@users.noreply.github.com>
2025-06-10 18:41:51 +0000

    variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,30 @@
+const accountId = 7766071191
+let accountEmail = "merahullogan7@gmail.com"
+var accountPassword = "123456"
+
+
+// accountId = 7766
+
+accountcity = "aurangabad"
+
+console.log(accountId);
+
+accountEmail = "merahullogon7@gmail.com"
+accountPassword = "77660"
+accountcity = "patna"
+
+let accountstate;
+
+console.table([accountId, accountEmail, accountPassword, accountcity, accountstate]);
+
+// Variables are Containers for Storing Data
+
+
+/* JavaScript Variables can be declared in 4 ways:
+
+Automatically
+Using var
+Using let
+Using const
+
+*/
