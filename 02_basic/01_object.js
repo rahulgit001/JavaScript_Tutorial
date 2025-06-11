a4c4eecf656519435936e8159643e3813950ef9c
Rahul kumar <131306879+rahulgit001@users.noreply.github.com>
2025-06-11 22:31:48 +0000

    01_object

---
+++ 02_basic/01_object.js
@@ -0,0 +1,34 @@
+// singleton 
+
+// Real Life Objects
+
+const user = {
+    name: "Rahul",
+    lname: "kumar",
+    "fullname": "rahul kuamar",
+    // [mySym]: "mykey1",
+    age: 24,
+    location: 'aurangabad',
+    email: "merahullogan7@gmail.com",
+    mobile: 7766071191,
+}
+
+console.log(user.email)
+console.log(user["email"])
+console.log(user["fullname"])
+// console.log(user[mySym])
+
+user.email = "merahullogan7@gmail.com"
+// Object.freeze(user)
+user.email = "merahul7@gamil.com"
+console.log(user);
+
+user.greeting = function () {
+    console.log("Hello js Rahul");
+}
+
+user.greetingtwo = function () {
+    console.log(`Hello js Rahul, ${this.lname}`);
+}
+console.log(user.greeting());
+console.log(user.greetingtwo());
