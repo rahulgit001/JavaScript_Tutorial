8b0640afcf9231ed39fadf07ebe3629dacb98ca3
Rahul kumar <131306879+rahulgit001@users.noreply.github.com>
2025-06-11 07:40:31 +0000

    DataType

---
+++ 01_basic/01_DataTypes.js
@@ -0,0 +1,61 @@
+/*
+"use strict"  // treat all js code as newer version
+String
+Number
+Bigint
+Boolean
+Undefined
+Null
+Symbol
+Object
+*/
+
+// 1. Primitive Data Types
+
+let name = "hitesh" //string
+let age = 25        // number
+let num = false     // boolean
+let x = BigInt("123456789012345678901234567890"); // bigint
+let account;   // undifind
+let space = null // null standalone value
+let id = Symbol('id');    // Symbol
+
+console.log(typeof (num));
+
+//  2. Non-Primitive (Reference) Data Types
+
+const persion = {
+    name: "rahul",
+    lastname: "kumar",
+    address: "aurangabad",
+    email: "merahullogan7@gmail.com",
+    mobile: 7766071191,
+    account: "",
+
+}
+
+console.log(persion);
+console.log(typeof account);
+console.log(typeof persion.mobile);
+
+let count = "88as"
+let value = Number(count)
+console.log(typeof value);
+console.log(value)
+
+// "88"  => 88
+// "88acs" => NaN
+// true => 1; false => 0
+
+let is = 1
+let booleanis = Boolean(is)
+console.log(booleanis);
+
+
+// 1 => true; 0 => false
+//  "" => false
+//  "rahul" => true
+
+let sumnumber = 77
+let stringNumber = String(sumnumber)
+console.log(stringNumber)
