b56a183f27503b37caf48dab88a90fab69b9029b
Rahul kumar <131306879+rahulgit001@users.noreply.github.com>
2025-06-11 12:09:48 +0000

    operators

---
+++ 01_basic/01_Operators.js
@@ -0,0 +1,59 @@
+// Javascript operators are used to perform different types of mathematical and logical computations.
+
+let calculation = 55
+// let coll = -value
+
+// console.log(nagivet)
+
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 ** 3);
+console.log(2 / 3);
+console.log(2 % 3);
+
+let num1 = "hello"
+let num2 = " rahul"
+let num = num1 + num2
+console.log(num);
+
+console.log("1" + 2);
+console.log(1 + "2");
+
+// comparisition operater
+
+console.log(10 > 5);
+console.log(10 >= 5);
+
+console.log("5" == 5);
+
+// ternary operator
+
+let age = 25;
+let status1 = (age > 18) ? 'i can voat' : 'i can note voat';
+
+console.log(status1);
+
+//  Logical operator
+
+let answer = (true && true && true)
+console.log(answer);
+
+let ans = (false || false || true)
+console.log(ans);
+
+let aws = !(true);
+console.log(aws);
+
+console.log(false || 'rahul');
+console.log(false || 7  || 10 ||18);
+
+//  bitwise operator
+
+console.log(2 & 5);
+console.log(2 | 5);
+console.log(~(0));
+console.log(2 ^ 2);
+
+console.log(10 >> 1);
+console.log(10 << 1);
