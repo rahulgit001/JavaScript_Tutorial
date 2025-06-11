ebc5466486419b8d60fb61325c531198779b6936
Rahul kumar <131306879+rahulgit001@users.noreply.github.com>
2025-06-11 07:56:04 +0000

    conversition

---
+++ 01_basic/02_DataTypes.js
@@ -0,0 +1,15 @@
+let age = 44
+  // "44abc",  null, undefiend, true  "rahul",  
+console.log(typeof age);
+console.log(typeof (age));
+
+let numnumber = Number(age)
+console.log(typeof numnumber);
+console.log(numnumber)
+
+let numin = 4    // "rahul", 4, "", 
+let booleanin = Boolean(numin)
+console.log(booleanin);
+
+// 1 => true;, 0 => falsh
+// "" => falsh "rahul" => true
