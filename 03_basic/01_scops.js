acca8a77599d529567405b1ca4ceaeb2cee1d350
Rahul kumar <131306879+rahulgit001@users.noreply.github.com>
2025-06-13 03:37:29 +0000

    01_scops

---
+++ 03_basic/01_scops.js
@@ -0,0 +1,20 @@
+let num1 = 10
+const num2 = 30
+var num3 = 20
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+
+// var c = 90
+let a = 300
+if (true){
+    let a = 10
+    const b = 20
+    // var c = 30
+    console.log("inner: ", a);
+}
+
+console.log(a);
+// console.log(b);
+// console.log(c);
