d81c7834e01f05a9add1115c012c8ddcde1ebc63
Rahul kumar <131306879+rahulgit001@users.noreply.github.com>
2025-06-13 01:39:19 +0000

    03_object

---
+++ 02_basic/_03_object.js
@@ -0,0 +1,17 @@
+const course = {
+    coursename: "javascript",
+    price: "999",
+    courseinstructor: "rahul"
+}
+
+const {courseinstructor: instructor} = course
+
+console.log(instructor);
+
+// {
+//     "names": "rahul",
+//     "coursenames": "javasscript",
+//     "pricr": "free"
+
+// }
+
