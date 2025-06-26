afd5996569fc577ddba1d4171e6dde0890a42280
Rahul kumar <131306879+rahulgit001@users.noreply.github.com>
2025-06-26 09:50:56 +0000

    iife

---
+++ 03_basic/01_iife.js
@@ -0,0 +1,13 @@
+//  immediately Invoked function expression (IIFE)
+
+// JavaScript functions are defined with the function keyword.
+
+// You can use a function declaration or a function expression.
+
+(function name(){
+    console.log(`Rahul`)
+})();
+
+( () => {
+    console.log(`rahul`)
+})()
